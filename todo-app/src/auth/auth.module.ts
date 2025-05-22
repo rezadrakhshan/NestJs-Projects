ec424f77b25ae3c1b0e27016a95bc159817c78ec
unknown <github2reza@gmail.com>
2025-05-22 18:42:31 +0330

    feat(Authentication): Dveloped 2 endpoint for login and register

--- todo-app/src/auth/auth.module.ts
+++ todo-app/src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './schema/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: userSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- todo-app/src/auth/auth.service.ts
+++ todo-app/src/auth/auth.service.ts
@@ -1,15 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schema/user.schema';
+import { pick } from 'lodash';
+import { AuthData } from './interfaces/login.interface';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-  async register(data) {
-    return data
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private JwtService: JwtService,
+  ) {}
+  async register(data): Promise<User> {
+    let value: AuthData = pick(data, ['username', 'password']);
+    let user = await this.userModel.findOne({ username: value.username });
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const saltRounds = 10;
+    value.password = await bcrypt.hash(value.password, saltRounds);
+    user = new this.userModel(value);
+    return await user.save();
   }
-  async login(data){
-    return data
+  async login(data): Promise<{ access_token: string }> {
+    const value: AuthData = pick(data, ['username', 'password']);
+    const target = await this.userModel.findOne({ username: value.username });
+    if (!target) {
+      throw new UnauthorizedException();
+    }
+    const isMatch = await bcrypt.compare(value.password, target.password);
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: target.id };
+    return {
+      access_token: await this.JwtService.signAsync(payload),
+    };
   }
 }
