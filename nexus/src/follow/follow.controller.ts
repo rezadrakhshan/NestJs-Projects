578730ef47ad73edc0d540bdf6564987d6453029
unknown <github2reza@gmail.com>
2025-06-28 16:03:20 +0330

    feat(folow): Developed 2 ednpoints for follow and unfollow user

--- nexus/src/follow/follow.controller.ts
+++ nexus/src/follow/follow.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post,Delete, Param, Req } from '@nestjs/common';
+import { FollowService } from './follow.service';
+import { Request } from 'express';
 
 @Controller('follow')
-export class FollowController {}
+export class FollowController {
+  constructor(private followService: FollowService) {}
+
+  @Post(':id')
+  async followUser(@Param('id') id: string, @Req() req: Request) {
+    return this.followService.followUser(id, req);
+  }
+  @Delete(':id')
+  async unFollowUser(@Param('id') id: string, @Req() req: Request) {
+    return this.followService.unFollowUser(id, req);
+  }
+}
--- nexus/src/follow/follow.service.ts
+++ nexus/src/follow/follow.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Follow } from './schemas/follow.schema';
+import { User } from 'src/auth/schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+  constructor(
+    @InjectModel(Follow.name) private followModel: Model<Follow>,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+  async followUser(id, req) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Invalid userID');
+    }
+    const following = await this.userModel.findById(id);
+    if (!following) {
+      throw new NotFoundException('User does not exists');
+    }
+    const newFollow = await new this.followModel({
+      follower: req.user.sub,
+      following: id,
+    });
+    await newFollow.save();
+    return newFollow;
+  }
+  async unFollowUser(id, req) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Invalid userID');
+    }
+    const following = await this.userModel.findById(id);
+    if (!following) {
+      throw new NotFoundException('User does not exists');
+    }
+    const newFollow = await this.followModel.deleteOne(
+      {
+        follower: req.user.sub,
+        following: id,
+      }
+    );
+    return newFollow;
+  }
+}
