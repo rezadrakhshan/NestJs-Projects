7f6d05428badbaeaaf59ebeae637251732e10eb6
unknown <github2reza@gmail.com>
2025-07-05 17:39:19 +0330

    feat(core): add Htt exception filter

---
+++ nexus/src/decorator/http-exception.filter.ts.ts
@@ -0,0 +1,31 @@
+import { Catch, ExceptionFilter, ArgumentsHost, HttpException } from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status = exception.getStatus();
+
+    const exceptionResponse = exception.getResponse();
+    let message = '';
+
+    if (typeof exceptionResponse === 'string') {
+      message = exceptionResponse;
+    } else if (
+      typeof exceptionResponse === 'object' &&
+      exceptionResponse !== null &&
+      'message' in exceptionResponse
+    ) {
+      message = (exceptionResponse as any).message;
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
