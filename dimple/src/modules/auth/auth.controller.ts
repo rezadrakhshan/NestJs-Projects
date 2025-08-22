25cb2c4b2eecf1c5481c0f895df341d2c3295b03
Derakhshan <github2reza@gmail.com>
2025-08-22 18:32:02 +0330

    feat(authentication): dveleoped auth guard

--- dimple/src/modules/auth/auth.controller.ts
+++ dimple/src/modules/auth/auth.controller.ts
@@ -8,17 +8,20 @@ import {
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.sto';
 import { LoginDto } from './dto/login.dto';
+import { Public } from 'src/decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('register')
   @UsePipes(new ValidationPipe())
   async register(@Body() data: RegisterDto) {
     return this.authService.register(data);
   }
 
+  @Public()
   @Post('login')
   @UsePipes(new ValidationPipe())
   async login(@Body() data: LoginDto) {
--- dimple/src/modules/auth/auth.module.ts
+++ dimple/src/modules/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/database/entity/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/common/auth.guard';
 
 @Module({
   imports: [
@@ -18,6 +20,6 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AuthModule {}
