9feac78287a66303116174146d3171d149d92c9a
Derakhshan <github2reza@gmail.com>
2025-10-18 18:50:33 +0330

    feat(database): Configured TypeOrm and auth module

--- link-up/src/app.module.ts
+++ link-up/src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
