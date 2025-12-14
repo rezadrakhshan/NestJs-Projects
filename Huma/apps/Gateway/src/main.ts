60637c399b045299834134bf71b5ea4107b2c53c
Derakhshan <github2reza@gmail.com>
2025-12-14 13:19:53 +0330

    feat(gateway): Initital Config Service

--- Huma/apps/Gateway/src/main.ts
+++ Huma/apps/Gateway/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port')
+
+  await app.init();
+  server.listen(port, () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
